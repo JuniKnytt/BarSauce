131820d2d00584c55f72b53802979f219a08f5be
Jmarsaj <jerinsajimon@gmail.com>
2022-09-29 18:29:47 -0700

    barcodeDecoder function
    Added function that is able to take an image, find the barcode, and decode it. Also prints it.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-print("BarSauce")
-print("BarSauce2")
-print("BarSauce3")
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+def barcodeDecoder(image):
+
+    img = cv2.imread(image)             #reads the image in numpy array
+    barcodes = decode(img)              #decodes barcode 
+    for obj in barcodes:       
+
+        (x, y, w, h) = obj.rect         #locating barcode in image
+        #cv2.rectangle(img, (x-10, y-10), (x+(w+10), y+(h+10)), (255, 0, 0), 2)
+
+        #if obj.data!="":
+        print(obj.data)
+        print(obj.type)
+
+image = "barcode.jpg"
+barcodeDecoder(image)
